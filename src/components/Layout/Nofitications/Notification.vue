e7ac4eaeca903354646accd2c932f6aac6a5da51
dairai <dainiusrainys99@gmail.com>
2025-07-16 17:56:52 +0300

    Initialized notifictionsList and notification components

---
+++ src/components/Layout/Nofitications/Notification.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="relative flex flex-row bg-white p-4">
+        <button class="bg-red-800 h-4 w-2"></button>
+        <span>Įmonės redaguoti nepavyko!</span>
+    </div>
+</template>
---
+++ src/components/Layout/Nofitications/NotificationList.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="fixed top-20 right-40 flex flex-col gap-4 m-auto">
+        <Notification />
+        <Notification />
+    </div>
+</template>
+<script setup lang="ts">
+import Notification from './Notification.vue';
+</script>
