24ef8cd412863f5d61a4c1514b2a123065119472
Garieldas <dainiusrainys99@gmail.com>
2025-07-16 21:42:13 +0300

    Created notifications list component. Created nofification component

--- src/components/Layout/Nofitications/NotificationList.vue
+++ src/components/Layout/Nofitications/NotificationList.vue
@@ -1,9 +1,13 @@
 <template>
-    <div class="fixed top-20 right-40 flex flex-col gap-4 m-auto">
-        <Notification />
-        <Notification />
+    <div class="fixed top-25 right-5 flex flex-col gap-4 m-auto w-80">
+        <Notification v-for="notification in store.notifications" :key="notification.id"
+        :notification="notification"
+        @remove-message="selectId"/>
     </div>
 </template>
 <script setup lang="ts">
 import Notification from './Notification.vue';
+import { useNotificationStore } from '@/stores/notificationstore';
+const store = useNotificationStore();
+const selectId = (id: string) => store.removeNotification(id);
 </script>
