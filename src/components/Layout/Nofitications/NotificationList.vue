b1b4b7a946a7be7fc7366b5fc3cea4236fced8c7
dairai <dainiusrainys99@gmail.com>
2025-07-17 10:49:40 +0300

    Added readonly field for notification, edited methods to showSuccessNotification and error

--- src/components/Layout/Nofitications/NotificationList.vue
+++ src/components/Layout/Nofitications/NotificationList.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="fixed top-25 right-5 flex flex-col gap-4 m-auto w-80">
-        <Notification v-for="notification in store.notifications" :key="notification.id"
+        <Notification v-for="notification in store._notifications" :key="notification.id"
         :notification="notification"
         @remove-message="selectId"/>
     </div>
@@ -10,4 +10,8 @@ import Notification from './Notification.vue';
 import { useNotificationStore } from '@/stores/notificationstore';
 const store = useNotificationStore();
 const selectId = (id: string) => store.removeNotification(id);
+store.addSuccessNotification('hi')
+store.addSuccessNotification('hi')
+store.addSuccessNotification('hi')
+store.addSuccessNotification('hi')
 </script>
--- src/stores/notificationstore.ts
+++ src/stores/notificationstore.ts
@@ -1,21 +1,35 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { readonly, ref } from "vue";
 import { v4 as uuidv4 } from 'uuid';
 import type { Notification } from "@/types/notificationType";
 
 export const useNotificationStore = defineStore('notifications', () => {
     const notifications = ref<Notification[]>([]);
+    const _notifications = readonly(notifications);
 
-    const addNotification = (notification:Notification) => {
+    const addSuccessNotification = (message: string) => {
         const id = uuidv4();
-        notification = {...notification, id};
+        const type = 'success';
+        const notification = {id, type, message} as Notification;
         notifications.value.push(notification)
         setTimeout(() => {
             notifications.value = notifications.value.filter((item) => item.id !== id);
         }, 3000)
     }
+
+    const addErrorNotification = (message: string) => {
+        const id = uuidv4();
+        const type = 'error';
+        const notification = {id, type, message} as Notification;
+        notifications.value.push(notification);
+        setTimeout(() => {
+            notifications.value = notifications.value.filter((item) => item.id !== id);
+        }, 3000)
+    }
+
     const removeNotification = (id: string) => {
         notifications.value = notifications.value.filter((item) => item.id !== id);
     }
-    return { notifications, addNotification, removeNotification }
+    
+    return { _notifications, addSuccessNotification, addErrorNotification, removeNotification }
 })
