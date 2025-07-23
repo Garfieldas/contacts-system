98c70bbaf2f2bed16872627b6f573a4b615a43d1
dairai <dainiusrainys99@gmail.com>
2025-07-23 10:08:21 +0300

    Refactored composables

--- src/composables/useCompanies.ts
+++ src/composables/useCompanies.ts
@@ -1,33 +1,21 @@
 import { getCompanies } from "@/services/companiesService";
 import { useNotificationStore } from "@/stores/notificationstore";
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
 
 export const useCompanies = () => {
     const companies = ref([]);
-    const page = ref();
-    const perPage = ref();
-    const totalItems = ref();
-    const totalPages = ref();
     const store = useNotificationStore();
 
     const fetchCompanies = async () => {
         try {
             const response = await getCompanies();
             companies.value = response.items;
-            page.value = response.page;
-            perPage.value = response.perPage;
-            totalItems.value = response.totalItems;
-            totalPages.value = response.totalPages;
         }
         catch (error: any) {
             store.addErrorNotification(error);
         }
     }
-    onMounted(() => {
-        fetchCompanies();
-    })
     return {
-        companies, page, perPage,
-        totalItems, totalPages
+        companies, fetchCompanies
     }
 }
