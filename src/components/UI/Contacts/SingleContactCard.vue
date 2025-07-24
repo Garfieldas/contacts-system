eb16a450e14f8b3557961080908a49243608e9ee
Garieldas <dainiusrainys99@gmail.com>
2025-07-25 00:09:42 +0300

    Created redirect to 404 if contact id do not exist. Styled 404 page. created employee type. Displayed employee values to card

--- src/components/UI/Contacts/SingleContactCard.vue
+++ src/components/UI/Contacts/SingleContactCard.vue
@@ -1,14 +1,20 @@
 <template>
-        <div class="bg-white rounded-lg shadow-md p-8 max-w-4xl mx-auto">
+    <div v-if="!employee" class="flex flex-col items-center justify-center w-full py-16 px-4 bg-gray-50 rounded-xl shadow-inner text-center">
+      <img src="../../../assets/icons/zero-results.svg" alt="No Results" class="w-24 h-24 mb-6 opacity-60"/>
+      <h1 class="text-2xl font-semibold text-gray-700 mb-2">Kontaktas nerastas</h1>
+      <p class="text-gray-500">Pabandykite iš naujo.</p>
+    </div>
+        <div class="bg-white rounded-lg shadow-md p-8 max-w-4xl mx-auto" v-else>
             <div class="flex items-center mb-6">
                 <div class="w-16 h-16 bg-gray-300 rounded-full flex items-center justify-center mr-6">
-                    <svg class="w-10 h-10 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <svg class="w-10 h-10 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" v-if="!employeePhotoUrl">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                     </svg>
+                <img v-else="employee.photo" :src="employeePhotoUrl" :alt="`Employee photo of ${employee.name} ${employee.surname}`" class="w-full h-full object-cover rounded-full">
                 </div>
                 <div>
-                    <h3 class="font-semibold text-gray-900 text-2xl">{{ employee.name }}</h3>
-                    <p class="text-lg text-gray-600">Pozicija: pozicijos_pavadinimas</p>
+                    <h3 class="font-semibold text-gray-900 text-2xl">{{ employee.name }} {{ employee.surname }}</h3>
+                    <p class="text-lg text-gray-600">Pozicija: {{ employee.position }}</p>
                 </div>
             </div>
 
@@ -18,11 +24,11 @@
                     <div class="space-y-2">
                         <div class="flex">
                             <span class="font-medium text-gray-500 w-28 flex-shrink-0">Elektroninis paštas:</span>
-                            <a href="mailto:example@teltonika.lt" class="text-[#1F3F77] hover:underline">example@teltonika.lt</a>
+                            <a href="mailto:example@teltonika.lt" class="text-[#1F3F77] hover:underline">{{ employee.email }}</a>
                         </div>
                         <div class="flex">
                             <span class="font-medium text-gray-500 w-28 flex-shrink-0">Telefono numeris:</span>
-                            <span>+00000000000</span>
+                            <span>{{ employee.phone_number }}</span>
                         </div>
                     </div>
                 </div>
@@ -32,23 +38,25 @@
                     <div class="space-y-2">
                         <div class="flex">
                             <span class="font-medium text-gray-500 w-28 flex-shrink-0">Kompanija:</span>
-                            <span>kompanijos_pavyzdys</span>
+                            <span>{{ employee.expand.company_id.name }}</span>
                         </div>
                         <div class="flex">
                             <span class="font-medium text-gray-500 w-28 flex-shrink-0">Būstinė:</span>
-                            <span>būstinės_pavyzdys</span>
+                            <span>{{ employee.expand.office_id.name }}</span>
                         </div>
                         <div class="flex">
                             <span class="font-medium text-gray-500 w-28 flex-shrink-0">Padalinys:</span>
-                            <span>padalinio_pavyzdys</span>
+                            <span>{{ employee.expand.division_id.name }}</span>
                         </div>
                         <div class="flex">
                             <span class="font-medium text-gray-500 w-28 flex-shrink-0">Skyrius:</span>
-                            <span>skyriaus_pavyzdys</span>
+                            <span v-if="employee.expand.department_id">{{ employee.expand.department_id.name }}</span>
+                            <span v-else>Nėra informacijos</span>
                         </div>
                         <div class="flex">
                             <span class="font-medium text-gray-500 w-28 flex-shrink-0">Grupė:</span>
-                            <span>grupės_pavyzdys</span>
+                            <span v-if="employee.expand.group_id">{{ employee.expand.group_id.name }}</span>
+                            <span v-else>Nėra informacijos</span>
                         </div>
                     </div>
                 </div>
@@ -56,5 +64,13 @@
         </div>
 </template>
 <script setup lang="ts">
+import { computed } from 'vue';
 const props = defineProps(['employee']);
+import { getPhotoUrl } from '@/services/employeesService';
+const employeePhotoUrl = computed(() => {
+    if (props.employee && props.employee.id && props.employee.photo) {
+        return getPhotoUrl(props.employee.id, props.employee.photo);
+    }
+    return '';
+});
 </script>
