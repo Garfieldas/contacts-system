299e4dab21989451ba8899cfef3f80347032db90
dairai <dainiusrainys99@gmail.com>
2025-07-24 08:55:23 +0300

    Handle emits in contacts view

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -24,19 +24,79 @@ import SearchBar from "@/components/UI/SearchBar.vue";
 import DisplayButton from "@/components/UI/Contacts/DisplayButton.vue";
 import PaginatioButton from "@/components/UI/Contacts/PaginatioButton.vue";
 import { useEmployees } from "@/composables/useEmployees";
-import { onMounted, shallowRef, ref } from "vue";
+import { onMounted, shallowRef, reactive, computed, watchEffect } from "vue";
 
 const { employees, totalItems, page, totalPages, perPage, fetchRequest } = useEmployees();
 
 const currentDisplay = shallowRef(ContactList);
-const company = ref();
 
 const toggleComponent = () => {
   currentDisplay.value = currentDisplay.value === ContactList ? ContactTable : ContactList;
 }
 
-const handleFilters = (filters: Object) => {
-  console.log(filters);
+const filters = reactive<{
+  company?: string;
+  office?: string;
+  division?: string;
+  department?: string;
+  group?: string;
+}>({});
+
+const filtersQuee = computed(() => {
+  const query: string[] = [];
+  if (filters.company) {
+    query.push(`company_id="${filters.company}"`)
+  }
+  if (filters.office) {
+    query.push(`office_id="${filters.office}"`)
+  }
+  if (filters.division) {
+    query.push(`division_id="${filters.division}"`)
+  }
+  if (filters.department) {
+    query.push(`department_id="${filters.department}"`)
+  }
+  if (filters.group) {
+    query.push(`group_id="${filters.group}"`)
+  }
+  let url = '';
+  if (query.length > 0){
+    url = `&filter=${encodeURIComponent(query.join(' && '))}&expand=office_id`;
+  }
+  else {
+    url = '&expand=office_id';
+  }
+  console.log(query)
+  return url;
+})
+
+const handleFilters = (newFilters: {
+  company?: string,
+  office?: string,
+  division?: string,
+  department?: string,
+  group?: string,
+}) => {
+  if(newFilters.company) {
+    filters.company = newFilters.company;
+    fetchRequest(filtersQuee.value)
+  }
+  if(newFilters.office) {
+    filters.office = newFilters.office;
+    fetchRequest(filtersQuee.value)
+  }
+  if(newFilters.division) {
+    filters.division = newFilters.division;
+    fetchRequest(filtersQuee.value)
+  }
+  if(newFilters.department) {
+    filters.department = newFilters.department;
+    fetchRequest(filtersQuee.value)
+  }
+  if(newFilters.group) {
+    filters.group = newFilters.group;
+    fetchRequest(filtersQuee.value)
+  }
 }
 
 onMounted(() => {
