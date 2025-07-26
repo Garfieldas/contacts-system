b34dec9f8e09ccbe71ca3551458190d378794cfc
Garieldas <dainiusrainys99@gmail.com>
2025-07-27 00:16:17 +0300

    Added icons for form inputs. Alligned button and form to each other. Added show and hide password functionality

--- src/components/UI/forms/LoginForm.vue
+++ src/components/UI/forms/LoginForm.vue
@@ -1,19 +1,22 @@
 <template>
-    <div class="flex justify-center px-6 mt-20 ">
-    <div class="bg-white">
-    <form class="flex flex-col gap-10 p-15 px-40">
+    <div class="bg-white mt-10 px-20 mb-10">
+    <form class="flex flex-col gap-10 p-10">
         <div class="flex flex-col p-10">
-        <h1 class="text-5xl font-medium">Admin Prisijungimas:</h1>
+        <h1 class="text-5xl font-small">Admin Prisijungimas:</h1>
         </div>
         <div class="flex flex-col">
             <label for="email">Elekotrinis paštas:</label>
             <input class="bg-gray-200 p-3 input-icon-email" type="email" id="email"
-             placeholder="Įveskite el.pašto adresą..."/>
+             placeholder="&#xf0e0; Įveskite el.pašto adresą..." required/>
         </div>
         <div class="flex flex-col">
-            <label for="password">Elekotrinis paštas:</label>
-            <input class="bg-gray-200 p-3 input-icon-email" type="password" id="password"
-             placeholder="Įveskite slaptažodį..."/>
+            <label for="password">Slaptažodis:</label>
+            <div class="relative"> <input class="bg-gray-200 p-3 input-icon-email w-full pr-10" :type="hidePassword ? 'password' : 'text'" id="password"
+                 placeholder="&#xf023; Įveskite slaptažodį..." required/>
+                <button type="button" class="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-600">
+                    <i :class="hidePassword ? 'fa fa-eye' : 'fa fa-eye-slash'" @click="showPassword"></i>
+                </button>
+            </div>
         </div>
         <router-link :to="{name: 'password-reset'}" class="text-sky-700 font-small">
             Slaptažodžio atkūrimas
@@ -21,8 +24,14 @@
         <div class="flex flex-col">
             <button class="bg-[#0054A6] text-white font-medium p-2.5 rounded-sm">Prisijungti</button>
         </div>
-        <div class="p-60"></div>
+        <div class="py-40"></div>
     </form>
     </div>
-    </div>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue';
+const hidePassword = ref(true);
+const showPassword = () => {
+    hidePassword.value = hidePassword.value ? false : true;
+}
+</script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,8 +1,18 @@
 <template>
+  <div class="min-h-screen flex flex-col justify-center items-center bg-[#1F3F77] relative">
+
+    <div class="absolute top-20 left-20">
+      <GoBackButton />
+    </div>
+
     <LoginForm />
+
+  </div>
 </template>
+
 <script setup lang="ts">
 import LoginForm from '@/components/UI/forms/LoginForm.vue';
+import GoBackButton from '@/components/UI/GoBackButton.vue';
 import { onMounted, onUnmounted } from 'vue';
 onMounted(() => {
     document.body.style.backgroundColor = '#1F3F77'
