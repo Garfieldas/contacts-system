0f4a3d7443b01906ce7692e8f216c0ae487d8ded
dairai <dainius.rainys@teltonika.lt>
2025-07-28 17:28:40 +0300

    Added logout functionality and created dropdown for icons

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -20,21 +20,40 @@
         <a href="#">Paskyros</a>
         </router-link>
         </div>
-        <div class="icon w-1/2 flex flex-row justify-end" v-if="store.isLoggedIn">
-            <img :src="UserPhotoUrl" alt="icon" v-if="UserPhotoUrl" class="w-10 bg-gray-100 rounded-full object-cover">
-            <img src="../../assets/icons/male-icon.png" alt="icon" v-else>
+        <div class="icon w-1/2 flex flex-row justify-end relative" v-if="store.isLoggedIn">
+            <img :src="UserPhotoUrl" alt="icon" v-if="UserPhotoUrl" class="w-10 bg-gray-100 rounded-full object-cover" @click="toggleDropDown">
+            <img src="../../assets/icons/male-icon.png" alt="icon" v-else @click="toggleDropDown">
+    <div class="absolute top-18 left-83 mt-3 bg-white rounded-md shadow-lg min-w-[180px] py-2 z-10 font-normal text-black" :class="{'hidden': hideDropDown}">
+        <a class="relative block px-4 py-2 text-sm hover:bg-gray-100 whitespace-nowrap group" href="#">
+            Pakeisti slaptažodį
+            <span class="absolute right-4 top-1/2 -translate-y-1/2 text-xs">▼</span>
+        </a>
+        <a class="block px-4 py-2 text-sm hover:bg-gray-100 whitespace-nowrap" @click="Logout">
+            Atsijungti
+        </a>
+    </div>
         </div>
     </nav>
 </template>
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useAuthenticationStore } from '@/stores/authenticationStore';
 import { getAvatarUrl } from '@/services/authenticationService';
+import { useNotificationStore } from '@/stores/notificationstore';
 const store = useAuthenticationStore();
+const notify = useNotificationStore();
 const UserPhotoUrl = computed(() => {
     if (store.user && store.user.id && store.user.avatar) {
         return getAvatarUrl(store.user.id, store.user.avatar);
     }
     return '';
 });
+const hideDropDown = ref(true);
+const toggleDropDown = () => {
+    hideDropDown.value = hideDropDown.value ? false : true
+}
+const Logout = () => {
+    store.Logout();
+    notify.addSuccessNotification('Jūs sėkmingai atsijungėte nuo savo paskyros.')
+}
 </script>
