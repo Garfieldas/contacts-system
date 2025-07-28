f02364a1aeca9672299f81b91e9dcbf65163062c
dairai <dainius.rainys@teltonika.lt>
2025-07-28 12:14:14 +0300

    Created password-resset service

--- src/services/authenticationService.ts
+++ src/services/authenticationService.ts
@@ -19,6 +19,16 @@ export const login = async (email: string, password: string) => {
     }
 }
 
+export const passwordResset = async (email: string) => {
+    try {
+        const response = await axiosInstance.post('/users/request-password-reset', email);
+        console.log(response);
+    }
+    catch (error: any){
+        throw error;
+    }
+}
+
 export const refreshToken = async () => {
     const token = localStorage.getItem('token');
     const store = useAuthenticationStore();
