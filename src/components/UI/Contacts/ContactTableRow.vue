dfad1d45e3d32a5d8d515f11fa9d3be04327f663
Garieldas <dainiusrainys99@gmail.com>
2025-07-30 22:54:21 +0300

    Added conditional table row and buttons based on user permissions

--- src/components/UI/Contacts/ContactTableRow.vue
+++ src/components/UI/Contacts/ContactTableRow.vue
@@ -10,15 +10,27 @@
           {{ hideEmail?  employee.email.slice(0, 30) : employee.email }} {{ arrow }}</td>
         <td class="px-6 py-4 text-[#1F3F77]" v-else>{{ employee.email }}</td>
         <td class="px-6 py-4">{{ employee.expand.office_id.name }}</td>
+        <td class="px-6 py-4 text-white" v-if="hideButtons">
+          <button class="bg-[#0054A6] rounded-2xl px-10 py-1 mr-4">Redaguoti</button>
+          <button class="bg-[#A61A11] rounded-2xl px-10 py-1">Ištrinti</button>
+        </td>
       </tr>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+import { useAuthenticationStore } from '@/stores/authenticationStore';
 const props = defineProps(['employee']);
 const hideEmail = ref(true);
 const arrow = ref('▼');
+const auth = useAuthenticationStore();
 const toggleEmail = () => {
   hideEmail.value = !hideEmail.value;
   arrow.value = hideEmail.value ? '▼' : '▲'
 }
+const hideButtons = computed(() => {
+    if (auth.isLoggedIn && auth.user_permissions.edit_employees && auth.user_permissions.delete_employees) {
+        return true
+    }
+    return false;
+})
 </script>
