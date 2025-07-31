cb055f447d15e7b84813a94f0fb7ca13f4f2b03a
dairai <dainius.rainys@teltonika.lt>
2025-07-31 09:48:47 +0300

    Created Base Modal and overlay components

--- src/components/UI/BaseModal.vue
+++ src/components/UI/BaseModal.vue
@@ -1,5 +1,14 @@
 <template>
-<div class="bg-white fixed">
-    <button class=""><img class="w-22" src="../../assets/icons/Close button.png"/></button>
+    <Overlay :show-overlay="showModal"/>
+<div class="bg-gray-200 fixed w-[300px] z-30 top-1/2 left-1/2" :class="{'hidden': !showModal}">
+    <div class="flex flex-row justify-end">
+    <button class="top-1"><img class="w-22" src="../../assets/icons/Close button.png"/></button>
+    </div>
+    <slot></slot>
 </div>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import Overlay from './Overlay.vue';
+const showModal = ref(false);
+</script>
