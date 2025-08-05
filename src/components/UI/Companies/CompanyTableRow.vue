2fdc6803c680dcabbe9f897b152fe0dbb9ac4ee7
Garieldas <dainiusrainys99@gmail.com>
2025-08-05 22:08:46 +0300

    Added user permisions rendering for companies

--- src/components/UI/Companies/CompanyTableRow.vue
+++ src/components/UI/Companies/CompanyTableRow.vue
@@ -1,7 +1,7 @@
 <template>
   <tr class="hover:bg-gray-50 transition-colors">
     <td class="px-6 py-4 text-gray-700">{{ company.name }}</td>
-    <td class="px-6 py-4 text-white flex justify-end pr-4">
+    <td class="px-6 py-4 text-white flex justify-end pr-4" v-if="hideActions">
       <button class="bg-[#0054A6] rounded-2xl px-10 py-1 mr-4 shadow-md hover:bg-blue-700 transition-colors" @click="emits('edit-company', company)">
         Redaguoti
       </button>
@@ -12,6 +12,16 @@
   </tr>
 </template>
 <script setup lang="ts">
+import { useAuthenticationStore } from '@/stores/authenticationStore';
+import { computed } from 'vue';
+
 const props = defineProps(['company']);
 const emits = defineEmits(['edit-company', 'delete-company']);
+const auth = useAuthenticationStore();
+const hideActions = computed(() => {
+  if(auth.isLoggedIn && auth.user_permissions.edit_companies && auth.user_permissions.delete_companies) {
+    return true;
+  }
+  return false;
+})
 </script>
