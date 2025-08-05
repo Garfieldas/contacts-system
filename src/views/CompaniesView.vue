2eb8e2d2cb9dfecff22533366310e966a4ecd190
garfieldas <dainiusrainys99@gmail.com>
2025-08-05 15:10:24 +0300

    Added permission check before displaying add new company

--- src/views/CompaniesView.vue
+++ src/views/CompaniesView.vue
@@ -1,6 +1,6 @@
 <template>
     <BaseLayout title="Įmonės">
-    <div class="flex flex-row items-center mb-6 gap-12">
+    <div class="flex flex-row items-center mb-6 gap-12" v-if="hideActions">
         <button>
         <img src="../assets/icons/Add Contact rounded.png" class="w-15 h-15"/>
         </button>
@@ -18,8 +18,17 @@ import BaseLayout from '@/components/Layout/BaseLayout.vue';
 import Pagination from '@/components/Layout/Pagination.vue';
 import CompanyTable from '@/components/UI/Companies/CompanyTable.vue';
 import { useCompanies } from '@/composables/useCompanies';
-import { onMounted, watch } from 'vue';
+import { useAuthenticationStore } from '@/stores/authenticationStore';
+import { computed, onMounted, watch } from 'vue';
 const { companies, fetchCompanies, page, totalPages, totalItems } = useCompanies();
+const auth = useAuthenticationStore();
+const hideActions = computed(() => {
+    if (auth.isLoggedIn && auth.user_permissions.edit_companies && auth.user_permissions.delete_companies) {
+        return true;
+    }
+    return false;
+})
+
 watch(page, () => {
     fetchCompanies();
 })
