412417f1d86112a72f3508e6560268bdd809f29d
dairai <dainius.rainys@teltonika.lt>
2025-08-06 08:45:15 +0300

    Created main view structure for company structure management

--- src/views/CompanyStructure/CompanyStructureManagement.vue
+++ src/views/CompanyStructure/CompanyStructureManagement.vue
@@ -1,3 +1,26 @@
 <template>
-    <h1>Company structure management</h1>
+    <BaseLayout title="Struktūra">
+    <div class="flex flex-row items-center mb-6 gap-12">
+        <button>
+        <img src="../../assets/icons/Add Contact rounded.png" class="w-15 h-15"/>
+        </button>
+        <h2>Pridėti naują struktūrą:</h2>
+    </div>
+    <div class="flex flex-row text-white gap-2">
+        <button class="bg-[#0054A6] border-2 border-transparent p-1.5 px-10 rounded-xl
+         hover:bg-white hover:text-[#0054A6] hover:border-[#0054A6]">Ofisai</button>
+
+        <button class="bg-[#0054A6] border-2 border-transparent p-1.5 px-10 rounded-xl
+        hover:bg-white hover:text-[#0054A6] hover:border-[#0054A6]">Padaliniai</button>
+
+        <button class="bg-[#0054A6] border-2 border-transparent p-1.5 px-10 rounded-xl
+        hover:bg-white hover:text-[#0054A6] hover:border-[#0054A6]">Skyriai</button>
+        
+        <button class="bg-[#0054A6] border-2 border-transparent p-1.5 px-10 rounded-xl
+        hover:bg-white hover:text-[#0054A6] hover:border-[#0054A6]">Grupės</button>
+    </div>
+    </BaseLayout>
 </template>
+<script setup lang="ts">
+import BaseLayout from '@/components/Layout/BaseLayout.vue';
+</script>
