b410d9019327ef7c299925fa78a416d9f968d72f
dairai <dainius.rainys@teltonika.lt>
2025-08-06 11:59:57 +0300

    Added validation before deleting company to check if it has any offices assigned to it

--- src/components/UI/forms/Companies/DeleteCompanyForm.vue
+++ src/components/UI/forms/Companies/DeleteCompanyForm.vue
@@ -18,11 +18,13 @@ import { useAuthenticationStore } from '@/stores/authenticationStore';
 import { useNotificationStore } from '@/stores/notificationstore';
 import { deleteCompany } from '@/services/companiesService';
 import { useEmployees } from '@/composables/useEmployees';
+import { useOffices } from '@/composables/useOffices';
 const props = defineProps(['company']);
 const emits = defineEmits(['company-deleted', 'cancel-delete'])
 const store = useNotificationStore();
 const auth = useAuthenticationStore();
 const { employees, fetchRequest, perPage } = useEmployees();
+const { offices, fetchOffices } = useOffices();
 perPage.value = '';
 
 const onSubmit = async () => {
@@ -30,9 +32,17 @@ const onSubmit = async () => {
         store.addErrorNotification('Nepakanka teisių šiai operacijai atlikti.');
         return;
     }
+
     await fetchRequest(`&filter=company_id="${props.company.id}"`);
     if (employees.value.length > 0) {
         store.addErrorNotification('Ši įmonė turi priskirtus kontaktus!');
+        emits('cancel-delete')
+        return;
+    }
+    
+    await fetchOffices(`?filter=company_id="${props.company.id}"`);
+    if (offices.value.length > 0) {
+        store.addErrorNotification('Ši įmonė turi priskirtus ofisus!');
         emits('cancel-delete')
         return;
     }
