6f661307f3d0f0251aeffe30c7f04158d0930cef
garfieldas <dainiusrainys99@gmail.com>
2025-08-09 08:38:02 -0700

    Created get divisions service

---
+++ src/services/divisionsService.ts
@@ -0,0 +1,13 @@
+import { axiosInstance } from "./axiosService";
+
+export const getDivisions = async (params?: string) => {
+    const url = params? `/divisions/records${params}` : '/divisions/records';
+    try {
+        const response = await axiosInstance.get(url);
+        const data = response.data;
+        return data;
+    }
+    catch (error: any) {
+        throw error;
+    }
+}
