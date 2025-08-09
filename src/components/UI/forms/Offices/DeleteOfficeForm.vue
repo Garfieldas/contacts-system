040f259d8b18b01ad4530ce30fbb58d320f6ffa7
garfieldas <dainiusrainys99@gmail.com>
2025-08-09 07:59:39 -0700

    Added validation to check if office have related companies before deletion

--- src/components/UI/forms/Offices/DeleteOfficeForm.vue
+++ src/components/UI/forms/Offices/DeleteOfficeForm.vue
@@ -1,16 +1,16 @@
 <template>
     <form class="flex flex-col justify-start p-2" @submit.prevent="onSubmit">
-    <h2 class="p-2 mb-2 text-xl">Ar tikrai norite ištrinti ofisą?</h2>
-    <div class="flex flex-col p-2 text-gray-500">
-        <div class="flex gap-2">
-            <span>Ofiso pavadinimas:</span>
-            <span>{{ office.name }} </span>
+        <h2 class="p-2 mb-2 text-xl">Ar tikrai norite ištrinti ofisą?</h2>
+        <div class="flex flex-col p-2 text-gray-500">
+            <div class="flex gap-2">
+                <span>Ofiso pavadinimas:</span>
+                <span>{{ office.name }} </span>
+            </div>
         </div>
-    </div>
-    <div class="flex flex-row justify-end gap-5 mt-10 text-[#0054A6]">
-        <a class="cursor-pointer uppercase hover:text-red-600" @click="emits('cancel-delete')">Ne</a>
-        <button type="submit" class="cursor-pointer uppercase hover:text-sky-400">Taip</button>
-    </div>
+        <div class="flex flex-row justify-end gap-5 mt-10 text-[#0054A6]">
+            <a class="cursor-pointer uppercase hover:text-red-600" @click="emits('cancel-delete')">Ne</a>
+            <button type="submit" class="cursor-pointer uppercase hover:text-sky-400">Taip</button>
+        </div>
     </form>
 </template>
 <script setup lang="ts">
@@ -18,7 +18,7 @@ import { useEmployees } from '@/composables/useEmployees';
 import { useAuthenticationStore } from '@/stores/authenticationStore';
 import { useNotificationStore } from '@/stores/notificationstore';
 import { getCompaniesOffices } from '@/services/companiesOfficesService';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 const props = defineProps(['office']);
 const auth = useAuthenticationStore();
 const store = useNotificationStore();
@@ -27,24 +27,35 @@ const emits = defineEmits(['cancel-delete']);
 const companiesOffices = ref();
 
 const fetchCompaniesOffices = async (params?: string) => {
-  const url = params ? `${params}` : '';
-  try {
-    const response = await getCompaniesOffices(url);
-    companiesOffices.value = response.items;
+    const url = params ? `${params}` : '';
+    try {
+        const response = await getCompaniesOffices(url);
 
-  }
-  catch (error: any) {
-    store.addErrorNotification(error);
-  }
+        if (response.items.length > 0) {
+            const associatedCompanies = response.items[0].expand.company_id;
+            companiesOffices.value = associatedCompanies;
+        } else {
+            companiesOffices.value = [];
+        }
+    }
+    catch (error: any) {
+        store.addErrorNotification(error);
+    }
 }
 const onSubmit = async () => {
-    if(!auth.isLoggedIn && !auth.user_permissions.delete_offices) {
+    if (!auth.isLoggedIn && !auth.user_permissions.delete_offices) {
         store.addErrorNotification('Nepakanka teisių šiai operacijai atlikti.');
         return;
     }
     await fetchRequest(`&filter=office_id="${props.office.id}"`);
-    if(employees.value.length > 0) {
+    if (employees.value.length > 0) {
         store.addErrorNotification('Šis ofisas turi priskirtus kontaktus');
+        emits('cancel-delete');
+        return;
+    }
+    await fetchCompaniesOffices(`?filter=office_id="${props.office.id}"&expand=company_id&fields=id,expand.company_id`);
+    if (companiesOffices.value.length > 0) {
+        store.addErrorNotification('Šis ofisas turi priskirtas įmonės');
         emits('cancel-delete');
         return;
     }
