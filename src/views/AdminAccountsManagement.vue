bee3d019fca35ff20a7672d74c22ad68c7e44aa5
Garieldas <dainiusrainys99@gmail.com>
2025-08-10 22:25:58 +0300

    Create Admin accounts view, created componens user table and table row components

--- src/views/AdminAccountsManagement.vue
+++ src/views/AdminAccountsManagement.vue
@@ -1,3 +1,12 @@
 <template>
-    <h1>Admin accounts management</h1>
+    <BaseLayout title="Admin paskyros">
+    <div class="flex items-center mb-6 gap-12" id="button">
+        <AddButton/>
+        <h2>Sukūrti naują admin paskyrą:</h2>
+    </div>
+    </BaseLayout>
 </template>
+<script setup lang="ts">
+import BaseLayout from '@/components/Layout/BaseLayout.vue';
+import AddButton from '@/components/UI/AddButton.vue';
+</script>
