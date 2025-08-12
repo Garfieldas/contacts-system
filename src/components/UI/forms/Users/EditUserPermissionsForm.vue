e10a6466d3f2f9522907fa5153829cf3d6c8cb06
Garieldas <dainiusrainys99@gmail.com>
2025-08-12 22:32:22 +0300

    Updated change permissions form layout

--- src/components/UI/forms/Users/EditUserPermissionsForm.vue
+++ src/components/UI/forms/Users/EditUserPermissionsForm.vue
@@ -1,20 +1,15 @@
 <template>
-  <form @submit.prevent="onSubmit">
-    <h2 class="text-3xl font-normal mb-6">Rėdaguoti paskyros teisės:</h2>
-
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
-      <div>
-        <h3 class="text-lg font-medium mb-4 text-center">Administracinės teisės:</h3>
-        <div class="flex flex-col justify-center">
-          <div class="mb-4 flex gap-5" v-for="(permission, key) in permissions">
-            <input type="checkbox" :id="key" v-model="selectedPermissions[key]"></input>
-            <label :for="key" :class="{ 'line-through': selectedPermissions[key] }">{{ permission }}</label>
-          </div>
-        </div>
+  <form @submit.prevent="onSubmit" class="flex flex-col items-center">
+    <h2 class="text-3xl text-center font-normal mb-6">Rėdaguoti paskyros teisės:</h2>
+    <h3 class="text-lg font-medium mb-6 text-center">Administracinės teisės:</h3>
+    <div class="flex flex-col items-start w-full max-w-md">
+      <div class="mb-4 flex gap-5 w-full justify-between items-center" v-for="(permission, key) in permissions">
+        <label :for="key" :class="{ 'line-through': selectedPermissions[key] }">{{ permission }}</label>
+        <input type="checkbox" :id="key" v-model="selectedPermissions[key]" class="ml-auto"></input>
       </div>
     </div>
 
-    <div class="flex justify-end pt-4">
+    <div class="flex justify-end pt-4 w-full max-w-md">
       <button
         class="ps-20 pe-20 py-2 bg-[#0054A6] text-white rounded-xs text-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
         Atnaujinti
