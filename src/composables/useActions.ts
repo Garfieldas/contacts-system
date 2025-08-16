18d30935b394c33911572626a6463a3e7f38ac2e
garfieldas <dainiusrainys99@gmail.com>
2025-08-16 13:17:54 -0700

    Fixed mistakes in services when passing parameters to query. Added time prevention issues for actions composable

--- src/composables/useActions.ts
+++ src/composables/useActions.ts
@@ -5,56 +5,56 @@ export const useActions = () => {
     const auth = useAuthenticationStore();
 
     const showEditContacts = computed(() => {
-        if(auth.isLoggedIn && auth.user_permissions.edit_employees) {
+        if(auth.isLoggedIn && auth.user_permissions?.edit_employees) {
             return true;
         }
         return false;
     })
 
     const showDeleteContacts = computed(() => {
-        if(auth.isLoggedIn && auth.user_permissions.delete_employees) {
+        if(auth.isLoggedIn && auth.user_permissions?.delete_employees) {
             return true;
         }
         return false;
     })
 
     const showEditCompanies = computed(() => {
-        if(auth.isLoggedIn && auth.user_permissions.edit_companies) {
+        if(auth.isLoggedIn && auth.user_permissions?.edit_companies) {
             return true;
         }
         return false;
     })
 
     const showDeleteCompanies = computed(() => {
-        if(auth.isLoggedIn && auth.user_permissions.delete_companies) {
+        if(auth.isLoggedIn && auth.user_permissions?.delete_companies) {
             return true;
         }
         return false;
     })
 
     const showEditOffices = computed(() => {
-        if (auth.isLoggedIn && auth.user_permissions.edit_offices) {
+        if (auth.isLoggedIn && auth.user_permissions?.edit_offices) {
             return true;
         }
         return false;
     })
 
     const showDeleteOffices = computed(() => {
-        if (auth.isLoggedIn && auth.user_permissions.delete_offices) {
+        if (auth.isLoggedIn && auth.user_permissions?.delete_offices) {
             return true;
         }
         return false;
     })
 
     const showEditStructure = computed(() => {
-        if (auth.isLoggedIn && auth.user_permissions.edit_structure) {
+        if (auth.isLoggedIn && auth.user_permissions?.edit_structure) {
             return true;
         }
         return false;
     })
 
     const showDeleteStructure = computed(() => {
-        if (auth.isLoggedIn && auth.user_permissions.delete_structure) {
+        if (auth.isLoggedIn && auth.user_permissions?.delete_structure) {
             return true;
         }
         return false;
